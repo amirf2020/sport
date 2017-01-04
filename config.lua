0b28466a55c7527db862eb077413424a44c2a78a
amirf2020 <arminghafouri8869.8869@gmail.com>
2017-01-04 12:47:38 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "279273406:AAHnckCT3rMcsU3SsLctK9yypGAGE83J2TU"
+bot_token = "247391558:AAH--Dd4W6dablPdJTKkf7ygKGN2-DObcIk"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.0"
-sudo_id = 180910633
+sudo_id = 1070278991
